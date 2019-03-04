d1a579fa3be9465c439a1b4c5b280fc91e7ee1a0
code-mds <43860962+code-mds@users.noreply.github.com>
2019-03-04 20:05:21 +0100

    es4

--- serie1/es4.cpp
+++ serie1/es4.cpp
@@ -1,11 +1,57 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-void aggiungi() {}
-void rimuovi() {}
-void lista() {}
-void modifica() {}
+enum class importanza { BASSO, MEDIO, ALTA, FATTO };
+
+struct Todo {
+    string titolo;
+    string descrizione;
+    importanza importanza;
+};
+
+void aggiungi() {
+    ofstream outputFile("Toto.dat");
+
+    Todo item;
+    cout << "Inserisci nuovi dati" << endl;
+    cout << "Titolo?" << flush;
+    cin >> item.titolo;
+    cout << "Descrizione?" << flush;
+    cin >> item.descrizione;
+    cout << "Importanza? [1 Bassa, 2 Media, 3 Alta, 4 Fatto]" << flush;
+    cin >> item.titolo;
+
+    char procedere;
+    cout << "Procedere? [S/N]" << flush;
+    cin.get(procedere);
+    if(procedere == 'S') {
+        outputFile << item.titolo << endl;
+        outputFile << item.descrizione << endl;
+        outputFile << int(item.importanza) << endl;
+        cout << "Dati modificati" << endl;
+    }
+    outputFile.close();
+}
+
+void rimuovi() {
+    cout << "Quale elemento vuoi rimuovere? (0 per annullare)" << flush;
+
+}
+
+void lista() {
+    ifstream inputFile("Toto.dat");
+    inputFile.close();
+}
+
+void modifica() {
+    cout << "======= Elenco elementi =======" << endl;
+    lista();
+    cout << "===============================" << endl;
+
+    cout << "Quale elemento vuoi modificare? (0 per annullare)" << flush;
+}
 
 void mainMenu() {
     char option;
